a88bd12f56c8d040b8bcf433682e15f80eb7790f
Sámuel Konderla <samuelkonderla@gmail.com>
2022-11-21 08:36:00 +0100

    Add project files.

---
+++ ok/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[] { 4, 2, 8, 1, 10, 7, 9, 5 };
+            int maxHely = 0;
+            int minHely = 0;
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                if (tomb[i] > tomb[maxHely])
+                {
+                    maxHely = i;
+                    
+                }
+                if (tomb[i] < tomb[minHely])
+                {
+                    minHely = i;
+
+                }
+            }
+
+            
+
+            Console.WriteLine($"Hol van: {maxHely}");
+            Console.WriteLine($"Melyik elem: {tomb[maxHely]}");
+
+            Console.WriteLine($"Hol van: {minHely}");
+            Console.WriteLine($"Melyik elem: {tomb[minHely]}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
